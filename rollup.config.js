98eafd1e19c5daaabfc3ad3ebea24ed013580c58
dragon <rongjihuang@gmail.com>
2017-01-04 12:31:40 +0800

    change structure

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import buble from 'rollup-plugin-buble';
 import postcss from 'rollup-plugin-postcss'
 import cssnano from 'cssnano'
 import uglify from 'rollup-plugin-uglify'
+import json from 'rollup-plugin-json'
 import md from 'rollup-plugin-md'
 
 var external = Object.keys(require('./package.json').dependencies);
@@ -11,6 +12,7 @@ export default {
   entry: 'src/components/index.js',
   external: external,
   plugins: [
+    json(),
     md(),
     vue(),
     postcss({
