5da90fa13aa6f6b0685f2a1b1757893a6126c5d0
RJ.Hwang <rongjihuang@gmail.com>
2017-01-05 17:59:41 +0800

    polishing

---
+++ src/chapters/01-about.md
@@ -0,0 +1,3 @@
+# 概述
+
+[Simter](https://github.com/simter) 团队开发的组件、应用、网站等的说明文档。本文档以开源的方式托管在 <https://github.com/simter/simter.github.io> 上。
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import './index.css'
 import layout from './layout.vue'
 import sidebar from './sidebar.vue'
-import readme from '../../README.md'
+import about from '../chapters/01-about.md'
 import sidebarItems from '../data/sidebar.json'
 
 new Vue({
@@ -12,13 +12,14 @@ new Vue({
     'app-sidebar': sidebar
   },
   data: {
-    mainContent: readme,
+    mainContent: about,
     sidebarItems: sidebarItems
   },
   methods: {
     clickSidebarItem: function (item) {
       console.log("clickSidebarItem=", JSON.stringify(item))
       console.log("all=", JSON.stringify(this.sidebarItems))
+      // 加载对应的页面内容到 main 区域中
     }
   }
 })
